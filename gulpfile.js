ab4f5149cf49c16055dd6ab16a5eb29898d6ceeb
Victor Fateh <victorfateh@yahoo.com>
2019-12-04 15:09:32 -0500

    Connected to firebase hosting

--- gulpfile.js
+++ gulpfile.js
@@ -3,29 +3,30 @@ const htmlmin = require("gulp-htmlmin");
 const cleanCSS = require("gulp-clean-css");
 const minify = require("gulp-minify");
 const imagemin = require("gulp-imagemin");
+const sitemap = require('gulp-sitemap');
 
 function html(cb) {
   return src("src/*.html")
-    .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(dest("dist"));
+  .pipe(htmlmin({ collapseWhitespace: true }))
+  .pipe(dest("dist"));
 }
 
 function js(cb) {
   return src("src/assets/js/*.js")
-    .pipe(minify())
-    .pipe(dest("dist/assets/js"));
+  .pipe(minify())
+  .pipe(dest("dist/assets/js"));
 }
 
 function css(cb) {
   return src("src/assets/css/*.css")
-    .pipe(cleanCSS({ compatibility: "ie8" }))
-    .pipe(dest("dist/assets/css"));
+  .pipe(cleanCSS({ compatibility: "ie8" }))
+  .pipe(dest("dist/assets/css"));
 }
 
 function bootstrapCSS() {
   return src("src/bootstrap3/**/*.css")
-    .pipe(cleanCSS({ compatibility: "ie8" }))
-    .pipe(dest("dist/bootstrap3"));
+  .pipe(cleanCSS({ compatibility: "ie8" }))
+  .pipe(dest("dist/bootstrap3"));
 }
 
 function bootstrapJS() {
@@ -34,8 +35,18 @@ function bootstrapJS() {
 
 function img() {
   return src("src/assets/img/**/*.*")
-    .pipe(imagemin())
-    .pipe(dest("dist/assets/img"));
+  .pipe(imagemin())
+  .pipe(dest("dist/assets/img"));
 }
 
-exports.default = parallel(html, css, bootstrapJS, bootstrapCSS, js, img);
+function sm() {
+  return src('src/*.html', {
+    read: false
+  })
+  .pipe(sitemap({
+    siteUrl: 'https://www.victorfateh.com'
+  }))
+  .pipe(dest('dist'));
+}
+
+exports.default = parallel(html, css, bootstrapJS, bootstrapCSS, js, img, sm);
